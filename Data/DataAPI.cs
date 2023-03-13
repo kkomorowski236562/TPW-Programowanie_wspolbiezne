fd2755a49ed3b7740e8cf53e16aaa29985c8c7d1
kkomorowski236562 <komorowski.kacper.18@gmail.com>
2023-03-13 13:00:30 +0100

    podział na warstwy, poczatek pracy

---
+++ Data/DataAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Data
+{
+    public abstract class DataAPI
+    {
+        public static DataAPI CreateApi()
+        {
+            return new DataAPIBase();
+        }
+    }
+
+    internal class DataAPIBase : DataAPI
+    {
+
+    }
+}
---
+++ Logic/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Logic
+{
+    internal class Ball
+    {
+        private readonly int size;
+        private int x;
+        private int y;
+        private int new_x;
+        private int new_y;
+
+        public Ball(int size, int x, int y, int new_x, int new_y)
+        {
+            this.size = size;
+            this.x = x;
+            this.y = y;
+            this.new_x = new_x;
+            this.new_y = new_y;
+        }
+        public int Size { get => size; }
+        public int X { get => x; }
+        public int Y { get => y; }
+
+        public void position(int lenght, int width)
+        {
+            if (x + new_x >= 0 && x + new_x <= lenght - size)
+            {
+                x += new_x;
+            }
+            else
+            {
+                if (new_x > 0)
+                {
+                    x = lenght - size;
+                }
+                else
+                {
+                    x = 0;
+                }
+                new_x *= -1;
+            }
+
+            if (y + new_y >= 0 && y + new_y <= width - size)
+            {
+                y += new_y;
+            }
+            else
+            {
+                if (new_y > 0)
+                {
+                    y = width - size;
+                }
+                else
+                {
+                    y = 0;
+                }
+                new_y *= -1;
+            }
+        }
+    }
+}
